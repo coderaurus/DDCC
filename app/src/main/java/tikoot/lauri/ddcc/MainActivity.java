05ff4a7bb3d118ed5975bcc95576003d89e91076
CodeRaurus <coderaurus@gmail.com>
2018-03-24 22:41:54 +0200

    Initial commit - MainActivity: initial class and xml - CreatorActivity: initial class and xml

---
+++ app/src/main/java/tikoot/lauri/ddcc/MainActivity.java
@@ -0,0 +1,28 @@
+package tikoot.lauri.ddcc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+/*
+ * Main Activity
+ * This activity serves as the main hub (menu).
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View view) {
+        if(view.getId() == R.id.menu_button_creator) {
+            Log.d("MainActivity", "Clicked the creator!");
+            Intent i = new Intent(this, CreatorActivity.class);
+            startActivity(i);
+        }
+    }
+}

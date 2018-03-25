e646ab1781e0d4f445f2d6659ffe493ee1e59b76
CodeRaurus <coderaurus@gmail.com>
2018-03-25 23:16:51 +0300

    Add integers.xml: initial commit Add CreatorActivity(layout): TODO: MenuBar comment

--- app/src/main/java/tikoot/lauri/ddcc/CreatorActivity.java
+++ app/src/main/java/tikoot/lauri/ddcc/CreatorActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class CreatorActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creator);
+
+
     }
 }

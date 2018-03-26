ef2ceb317cbeeec7aaf9a9c9457665c85a1a8e46
CodeRaurus <coderaurus@gmail.com>
2018-03-27 01:19:33 +0300

    Add DDCharacter(java): setSkills method implementation Add DDCharacter(java): applySkillProficiencies method implement

--- app/src/main/java/tikoot/lauri/ddcc/DDCharacter.java
+++ app/src/main/java/tikoot/lauri/ddcc/DDCharacter.java
@@ -7,12 +7,13 @@ package tikoot.lauri.ddcc;
  */
 
 public class DDCharacter {
+
     private String characterClass;
     private String race;
     private String background;
     private String alignment;
 
-    int level, proficiency;
+    int level, proficiency, health;
                                 // [cell 1] + [cell 2]
     int [][] attributes;        // attribute score + modifier
     int [][] attributeSaves;    // modfiers + proficiency
@@ -113,19 +114,47 @@ public class DDCharacter {
     private void setSkills(String attr) {
         switch (attr){
             case "str":
+                skills[3][0] = attributes[0][1];
                 break;
             case "dex":
+                skills[0][0] = attributes[1][1];
+                skills[15][0] = attributes[1][1];
+                skills[16][0] = attributes[1][1];
                 break;
             case "con":
                 break;
             case "int":
+                skills[2][0] = attributes[3][1];
+                skills[5][0] = attributes[3][1];
+                skills[8][0] = attributes[3][1];
+                skills[10][0] = attributes[3][1];
+                skills[14][0] = attributes[3][1];
                 break;
             case "wis":
+                skills[1][0] = attributes[4][1];
+                skills[6][0] = attributes[4][1];
+                skills[9][0] = attributes[4][1];
+                skills[11][0] = attributes[4][1];
+                skills[17][0] = attributes[4][1];
                 break;
             case "cha":
+                skills[4][0] = attributes[5][1];
+                skills[7][0] = attributes[5][1];
+                skills[12][0] = attributes[5][1];
+                skills[13][0] = attributes[5][1];
                 break;
             default:
                 break;
+        }
+        applySkillProficiencies();
+    }
+
+    private void applySkillProficiencies() {
+        for(int[] skill: skills) {
+            // 1 = has proficiency
+            if(skill[1] == 1) {
+                skill[0] += proficiency;
+            }
         }
     }
 }

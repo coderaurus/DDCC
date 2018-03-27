385c6a54f4694ed3fc732fe7fc4f7a86474bf55c
CodeRaurus <coderaurus@gmail.com>
2018-03-27 15:32:43 +0300

    Edit DDCharacter(java): setProficiency method implementation Edit DDCharacter(java): setAttribute method implementation Add DDCharacter(java): setAttributeModifier method implementation

--- app/src/main/java/tikoot/lauri/ddcc/DDCharacter.java
+++ app/src/main/java/tikoot/lauri/ddcc/DDCharacter.java
@@ -63,8 +63,22 @@ public class DDCharacter {
         return proficiency;
     }
 
-    public void setProficiency(int proficiency) {
-        this.proficiency = proficiency;
+    public void setProficiency() {
+        if(level >= 1 && level <= 4){
+            proficiency = 2;
+        }
+        else if(level >= 5 && level <= 8) {
+            proficiency = 3;
+        }
+        else if(level >= 9 && level <= 12) {
+            proficiency = 4;
+        }
+        else if(level >= 13 && level <= 16) {
+            proficiency = 5;
+        }
+        else if(level >= 17 && level <= 20) {
+            proficiency = 6;
+        }
     }
 
     public int[][] getAttributes() {
@@ -73,6 +87,91 @@ public class DDCharacter {
 
     public void setAttributes(int[][] attributes) {
         this.attributes = attributes;
+    }
+
+    public void setAttribute(String attr, int score){
+        switch (attr){
+            case "str":
+                attributes[0][0] = score;
+                setAttributeModifier(0, score);
+                break;
+            case "dex":
+                attributes[1][0] = score;
+                setAttributeModifier(1, score);
+                break;
+            case "con":
+                attributes[2][0] = score;
+                setAttributeModifier(2, score);
+                break;
+            case "int":
+                attributes[3][0] = score;
+                setAttributeModifier(3, score);
+                break;
+            case "wis":
+                attributes[4][0] = score;
+                setAttributeModifier(4, score);
+                break;
+            case "cha":
+                attributes[5][0] = score;
+                setAttributeModifier(5, score);
+                break;
+            default:
+                break;
+        }
+        setSkills(attr);
+    }
+
+    private void setAttributeModifier(int i, int score) {
+        int mod = 0;
+        if(score == 1){
+            mod = -5;
+        }
+        else if(score == 2 || score == 3){
+            mod = -4;
+        }
+        else if(score == 4 || score == 5){
+            mod = -3;
+        }
+        else if(score == 6 || score == 7){
+            mod = -2;
+        }
+        else if(score == 8 || score == 9){
+            mod = -1;
+        }
+        else if(score == 10 || score == 11){
+            mod = 0;
+        }
+        else if(score == 12 || score == 13){
+            mod = 1;
+        }
+        else if(score == 14 || score == 15){
+            mod = 2;
+        }
+        else if(score == 16 || score == 17){
+            mod = 3;
+        }
+        else if(score == 18 || score == 19){
+            mod = 4;
+        }
+        else if(score == 20 || score == 21){
+            mod = 5;
+        }
+        else if(score == 22 || score == 23){
+            mod = 6;
+        }
+        else if(score == 24 || score == 25){
+            mod = 7;
+        }
+        else if(score == 26 || score == 27){
+            mod = 8;
+        }
+        else if(score == 28 || score == 29){
+            mod = 9;
+        }
+        else if(score == 30){
+            mod = 10;
+        }
+        attributes[i][1] = mod;
     }
 
     public int[][] getAttributeSaves() {
@@ -89,26 +188,6 @@ public class DDCharacter {
 
     public void setSkills(int[][] skills) {
         this.skills = skills;
-    }
-
-    public void setAttribute(String attr, int score){
-        switch (attr){
-            case "str":
-                break;
-            case "dex":
-                break;
-            case "con":
-                break;
-            case "int":
-                break;
-            case "wis":
-                break;
-            case "cha":
-                break;
-            default:
-                break;
-        }
-        setSkills(attr);
     }
 
     private void setSkills(String attr) {

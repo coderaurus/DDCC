8b618142f34df18357aa61a311c7ceaef543bedc
CodeRaurus <coderaurus@gmail.com>
2018-04-18 00:09:45 +0300

    add DDCC_Utils.java: cleanup and javadoc

--- app/src/main/java/tikoot/lauri/ddcc/DDCC_Utils.java
+++ app/src/main/java/tikoot/lauri/ddcc/DDCC_Utils.java
@@ -1,33 +1,53 @@
 package tikoot.lauri.ddcc;
 
-import android.util.Log;
-
-import java.util.List;
 import java.util.Random;
 
 public class DDCC_Utils {
 
-    // Easier method to call for random number
-    // ( 1 - value ) both inclusive
+    /**
+     * Method returns a randomized value between 1 (inclusive) and <code>value</code>(inclusive).
+     *
+     * @param value the max value
+     * @return the randomized value
+     */
     public static int randomInt(int value){
         return new Random().nextInt(value) + 1;
     }
 
+    /**
+     * Method simulates a single dice roll.
+     *
+     * @param die the amount of sides in dice (4, 6, 8, ..)
+     * @return the result of the roll
+     */
     public static int rollDie(int  die) {
         Random rand = new Random();
         return rand.nextInt(die) + 1;
     }
 
+    /**
+     * Method simulates different kinds of die rolls used in roleplaying and returns the sum of the
+     * rolls. Method takes two bonus options currently:
+     * <ul>
+     *     <li>"smallest" to drop the smallest die off the result</li>
+     *     <li>"biggest" to drop the biggest die off the result</li>
+     * </ul>
+     * Note that <u>only one option is taken</u>, even though there is possibility to add more.
+     *
+     * @param die the amount of sides in the dice (4, 6, 8, ...)
+     * @param times how many times the dice is rolled
+     * @param options special rules
+     * @return the sum of the rolls
+     */
     public static int rollDies(int die, int times, String... options){
         int result = 0;
         // Any options given? => Cycle through them
         if(options.length > 0){
             for(String option : options){
-                int roll = 0;
                 if(option.equals("smallest")){      // Take smallest roll out of result
                     int smallest = 0;
                     for(int i=0; i<times; i++){
-                        roll = rollDie(die);
+                        int roll = rollDie(die);
                         if(i == 0 || roll < smallest){
                             smallest = roll;
                         }
@@ -39,7 +59,7 @@ public class DDCC_Utils {
                 else if(option.equals("biggest")){  // Take biggest roll out of result
                     int biggest = 0;
                     for(int i=0; i<times; i++){
-                        roll = rollDie(die);
+                        int roll = rollDie(die);
                         if(i == 0 || roll < biggest){
                             biggest = roll;
                         }
@@ -58,6 +78,13 @@ public class DDCC_Utils {
         return result;
     }
 
+    /**
+     * Method makes a string containing values of two-dimensional array.
+     * <i>Used for debug printing.</i>
+     *
+     * @param list two dimensional-array to print
+     * @return string containing values of the array
+     */
     public static String printList(int [][] list){
         String str = "{";
         int i=0;
@@ -69,7 +96,6 @@ public class DDCC_Utils {
             }
             i++;
         }
-
         str += "}";
 
         return str;

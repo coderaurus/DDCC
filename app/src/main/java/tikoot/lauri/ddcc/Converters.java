e1c85de2bc304df321c07c79107f9e034838f53c
CodeRaurus <coderaurus@gmail.com>
2018-04-23 03:31:11 +0300

    add Converters.java: javadoc

--- app/src/main/java/tikoot/lauri/ddcc/Converters.java
+++ app/src/main/java/tikoot/lauri/ddcc/Converters.java
@@ -10,14 +10,27 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class handles type conversions for database handling
+ */
 public class Converters {
 
+    /**
+     * Method returns a 2D List from a String
+     * @param value string to parse from
+     * @return List<List<String>>
+     */
     @TypeConverter
     public static List<List<String>> fromString(String value) {
         Type listType = new TypeToken<ArrayList<String>>() {}.getType();
         return new Gson().fromJson(value, listType);
     }
 
+    /**
+     * Method converts 2D List to String
+     * @param list convertable 2D List
+     * @return String representation of given List
+     */
     @TypeConverter
     public static String fromArrayList(List<List<String>> list) {
         Gson gson = new Gson();
@@ -26,6 +39,11 @@ public class Converters {
         return json;
     }
 
+    /**
+     * Method converts 2D integer array to a String
+     * @param array array to convert
+     * @return single String representating array
+     */
     @TypeConverter
     public static String fromIntegerTwoDimensionalArray(int[][] array){
         Gson gson = new Gson();
@@ -33,6 +51,11 @@ public class Converters {
         return json;
     }
 
+    /**
+     * Method converts String to 2D integer array
+     * @param value
+     * @return
+     */
     @TypeConverter
     public static int[][] fromStringToIntegerTwoDArray(String value){
         Type listType = new TypeToken<ArrayList<String>>() {}.getType();
